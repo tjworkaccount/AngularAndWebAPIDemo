bac3219ebca55b10287cddb0aa84f72de3f860f7
tjworkaccount <terrence.persaud@gmail.com>
2016-07-07 23:53:48 -0500

    Created Base Context and separated Data.Classes project

---
+++ Context/BaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace Context
+{
+    public class BaseContext<TContext>: DbContext where TContext:DbContext
+    {
+        static BaseContext()
+        {
+            Database.SetInitializer<TContext>(null);
+        }
+
+        protected BaseContext() : base("name=DbConnectionString")
+        {
+
+        }
+    }
+}
--- Data/Context/DataContext.cs
+++ Data/Context/DataContext.cs
@@ -3,20 +3,16 @@ using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Reflection;
+using Context;
 using Data.Classes;
 
 namespace Data.Context
 {
-    public class DataContext : DbContext
+    public class DataContext : BaseContext<DataContext> 
     {
         public DbSet<Users> Users { get; set; }
         public DbSet<Samples> Samples { get; set; }
         public DbSet<Statuses> Statuses { get; set; }
-
-        public DataContext() : base("name=DbConnectionString")
-        {
-
-        }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
